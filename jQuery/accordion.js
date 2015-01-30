8bd3fe5643fbb25c3372a92926013bf4e82589f5
Divyanshu Verma <divyanshu.verma@payu.in>
2015-01-30 17:57:35 +0530

    product.js and product.html fixed

--- jQuery/accordion.js
+++ jQuery/accordion.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
 
-	$("#accordion").accordion(
-		{active:false, collapsible:true});
-	$("#nestedAccordion").accordion(
-		{active:false, collapsible:true});
-	$("#accordion").accordion("option", "heightStyle", "content");
-	$("#nestedAccordion").accordion("option", "heightStyle", "content");
+    $("#accordion").accordion(
+        {active:false, collapsible:true});
+    $("#nestedAccordion").accordion(
+        {active:false, collapsible:true});
+    $("#accordion").accordion("option", "heightStyle", "content");
+    $("#nestedAccordion").accordion("option", "heightStyle", "content");
 
-	if(sessionStorage.getItem("itemName")){
-		$('#'+sessionStorage.getItem("itemName")).parent().css("background-color", "yellow");
-		sessionStorage.removeItem("itemName");
-	}
+    if(sessionStorage.getItem("itemName")){
+        $('#'+sessionStorage.getItem("itemName")).parent().css("background-color", "yellow");
+        sessionStorage.removeItem("itemName");
+    }
 
-	if($.isNumeric(sessionStorage.getItem("accordianSection"))){
-		$("#accordion").accordion("option", "active", parseInt(sessionStorage.getItem("accordianSection")));
-		sessionStorage.removeItem("accordianSection");
-	}
-	else if(sessionStorage.getItem("accordianSection") == "x"){
-		$("#accordion").accordion("option", "active", 0);
-		$("#nestedAccordion").accordion("option", "active", 0);
-		sessionStorage.removeItem("accordianSection");
-	}
-	
+    if($.isNumeric(sessionStorage.getItem("accordianSection"))){
+        $("#accordion").accordion("option", "active", parseInt(sessionStorage.getItem("accordianSection")));
+        sessionStorage.removeItem("accordianSection");
+    }
+    else if(sessionStorage.getItem("accordianSection") == "x"){
+        $("#accordion").accordion("option", "active", 0);
+        $("#nestedAccordion").accordion("option", "active", 0);
+        sessionStorage.removeItem("accordianSection");
+    }
+    
 
-	$('a').click(function(){
-		sessionStorage.setItem("itemName",$(this).attr('id'));
-		sessionStorage.setItem("accordianSection",$(this).parent().parent().parent().attr('id'));
-	});
+    $('a').click(function(){
+        sessionStorage.setItem("itemName",$(this).attr('id'));
+        sessionStorage.setItem("accordianSection",$(this).parent().parent().parent().attr('id'));
+    });
 
 
 });
